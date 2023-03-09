ce45549d4a3102060de72b95ba6e77e123e1fd69
drelocatelli <andressalocatelli@hotmail.com>
2023-03-09 12:49:03 -0300

    fix arrow not working

--- client/public/js/effects.js
+++ client/public/js/effects.js
@@ -7,11 +7,11 @@ class Effects {
         if(showing) {
             const leftArrow = document.createElement('img');
             leftArrow.title = 'Arrastar pra esquerda';
-            leftArrow.src = 'src/assets/img/arrow_left.png';
+            leftArrow.src = 'assets/img/arrow_left.png';
 
             const rightArrow = document.createElement('img');
             rightArrow.title = 'Arrastar pra direita';
-            rightArrow.src = 'src/assets/img/arrow_right.png';
+            rightArrow.src = 'assets/img/arrow_right.png';
 
             arrowsEl.appendChild(leftArrow);
             arrowsEl.appendChild(rightArrow);
